36b3043432c3e86b643fa639081bb8c97d22cfd4
heet003 <heet.b003@gmail.com>
2024-02-24 23:04:15 +0530

    new file:   assets/favicon/favicon.ico 	new file:   assets/fonts/NeueHaasDisplayBlack.ttf 	new file:   assets/fonts/NeueHaasDisplayBlackItalic.ttf 	new file:   assets/fonts/NeueHaasDisplayBold.ttf 	new file:   assets/fonts/NeueHaasDisplayBoldItalic.ttf 	new file:   assets/fonts/NeueHaasDisplayLight.ttf 	new file:   assets/fonts/NeueHaasDisplayLightItalic.ttf 	new file:   assets/fonts/NeueHaasDisplayMediu.ttf 	new file:   assets/fonts/NeueHaasDisplayMediumItalic.ttf 	new file:   assets/fonts/NeueHaasDisplayRoman.ttf 	new file:   assets/fonts/NeueHaasDisplayRomanItalic.ttf 	new file:   assets/fonts/NeueHaasDisplayThin.ttf 	new file:   assets/fonts/NeueHaasDisplayThinItalic.ttf 	new file:   assets/fonts/NeueHaasDisplayXThin.ttf 	new file:   assets/fonts/NeueHaasDisplayXThinItalic.ttf 	new file:   assets/fonts/NeueHaasDisplayXXThin.ttf 	new file:   assets/fonts/NeueHaasDisplayXXThinItalic.ttf 	new file:   assets/icons8-right-arrow-50.png 	new file:   assets/reshot-icon-arrow-right-GKTFPJDL97.svg 	new file:   assets/video.mp4 	new file:   index.html 	new file:   script.js 	new file:   style.css

---
+++ script.js
@@ -0,0 +1,26 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+let elemContainer = document.querySelector("#elem-container");
+let fixedImage = document.querySelector("#fixed-image");
+console.log(fixedImage);
+
+elemContainer.addEventListener("mouseenter", () => {
+  fixedImage.style.display = "block";
+});
+
+elemContainer.addEventListener("mouseleave", () => {
+  fixedImage.style.display = "none";
+});
+
+let imageURL;
+var elements = document.querySelectorAll(".element");
+elements.forEach((element) => {
+  element.addEventListener("mouseenter", () => {
+    imageURL = element.getAttribute("data-image");
+    fixedImage.style.backgroundImage = `url(${imageURL})`;
+    console.log(imageURL);
+  });
+});
