6ebe540ceaf3ee23cc573612bef991b791287b90
heet003 <heet.b003@gmail.com>
2024-02-25 14:54:46 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -3,24 +3,14 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-var menu = document.querySelector("nav h3");
-let fullscr = document.querySelector("#fullscr");
-let navImage = document.querySelector("nav img");
-var flag = 0;
-menu.addEventListener("click", () => {
-  if (flag == 0) {
-    fullscr.style.top = 0;
-    navImage.style.opacity = 0;
-    flag = 1;
-  } else {
-    fullscr.style.top = "-100%";
-    navImage.style.opacity = 1;
-    flag = 0;
-  }
-});
+var loader = document.querySelector("#loader");
+setTimeout(() => {
+  loader.style.top = "-100%";
+}, 3000);
 
 imageLoader();
 swiperAnimation();
+menuAnimation();
 
 function swiperAnimation() {
   var swiper = new Swiper(".mySwiper", {
@@ -50,5 +40,23 @@ function imageLoader() {
       fixedImage.style.backgroundImage = `url(${imageURL})`;
       console.log(imageURL);
     });
+  });
+}
+
+function menuAnimation() {
+  var menu = document.querySelector("nav h3");
+  let fullscr = document.querySelector("#fullscr");
+  let navImage = document.querySelector("nav img");
+  var flag = 0;
+  menu.addEventListener("click", () => {
+    if (flag == 0) {
+      fullscr.style.top = 0;
+      navImage.style.opacity = 0;
+      flag = 1;
+    } else {
+      fullscr.style.top = "-100%";
+      navImage.style.opacity = 1;
+      flag = 0;
+    }
   });
 }
