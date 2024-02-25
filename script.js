158424df4e9349a2621ce6c717265361e977117b
heet003 <heet.b003@gmail.com>
2024-02-25 12:39:38 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector("#main"),
-//   smooth: true,
-// });
-
-let elemContainer = document.querySelector("#elem-container");
-let fixedImage = document.querySelector("#fixed-image");
-
-elemContainer.addEventListener("mouseenter", () => {
-  fixedImage.style.display = "block";
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
 });
 
-elemContainer.addEventListener("mouseleave", () => {
-  fixedImage.style.display = "none";
-});
+imageLoader();
+swiperAnimation();
 
-let imageURL;
-var elements = document.querySelectorAll(".element");
-elements.forEach((element) => {
-  element.addEventListener("mouseenter", () => {
-    imageURL = element.getAttribute("data-image");
-    fixedImage.style.backgroundImage = `url(${imageURL})`;
-    console.log(imageURL);
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 40,
   });
-});
+}
+
+function imageLoader() {
+  let elemContainer = document.querySelector("#elem-container");
+  let fixedImage = document.querySelector("#fixed-image");
+
+  elemContainer.addEventListener("mouseenter", () => {
+    fixedImage.style.display = "block";
+  });
+
+  elemContainer.addEventListener("mouseleave", () => {
+    fixedImage.style.display = "none";
+  });
+
+  let imageURL;
+  var elements = document.querySelectorAll(".element");
+  elements.forEach((element) => {
+    element.addEventListener("mouseenter", () => {
+      imageURL = element.getAttribute("data-image");
+      fixedImage.style.backgroundImage = `url(${imageURL})`;
+      console.log(imageURL);
+    });
+  });
+}
