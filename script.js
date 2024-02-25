579428f5fbf6822d25c9269c35917c7d553b75c7
heet003 <heet.b003@gmail.com>
2024-02-25 13:53:56 +0530

    new file:   assets/delete.png 	new file:   assets/menu.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
+var menu = document.querySelector("nav h3");
+let fullscr = document.querySelector("#fullscr");
+let navImage = document.querySelector("nav img");
+var flag = 0;
+menu.addEventListener("click", () => {
+  if (flag == 0) {
+    fullscr.style.top = 0;
+    navImage.style.opacity = 0;
+    flag = 1;
+  } else {
+    fullscr.style.top = "-100%";
+    navImage.style.opacity = 1;
+    flag = 0;
+  }
+});
+
 imageLoader();
 swiperAnimation();
 
