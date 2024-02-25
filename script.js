c7b1137ece134d2e0d67bb55f7f798eba5cef758
heet003 <heet.b003@gmail.com>
2024-02-25 16:09:46 +0530

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -41,19 +41,24 @@ function imageLoader() {
 
 function menuAnimation() {
   var menu = document.querySelector("nav h3");
-  let fullscr = document.querySelector("#fullscr");
-  let navImage = document.querySelector("nav img");
-  var flag = 0;
+  var fullscr = document.querySelector("#fullscr");
+  var navImage = document.querySelector("nav img");
+  var flag = false;
+
   menu.addEventListener("click", () => {
-    if (flag == 0) {
+    if (!flag) {
       fullscr.style.top = "0";
+      fullscr.style.zIndex = "999";
       navImage.style.opacity = 0;
-      flag = 1;
+      fullscr.classList.toggle("open");
     } else {
       fullscr.style.top = "-200%";
+      fullscr.style.zIndex = "99";
       navImage.style.opacity = 1;
-      flag = 0;
+      fullscr.classList.toggle("closed");
     }
+    // flag = !flag;
+    navImage.style.opacity = fullscr.classList.contains("open") ? 0 : 1;
   });
 }
 
